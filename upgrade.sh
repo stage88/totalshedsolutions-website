64264791100e25299775dd0abf5d62c067a6ed18
Sam Ilic <stage88@gmail.com>
2024-11-30 12:15:19 +1100

    Package updates

--- upgrade.sh
+++ upgrade.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
 
+# Function to clean packages
+clean_packages() {
+  echo "Cleaning packages..."
+  npm run clean-packages
+}
+
+# Parse arguments
+while getopts "c" opt; do
+  case $opt in
+    c)
+      clean_packages
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+  esac
+done
+
 npm run clean
 ncu -u
-npm run clean-packages
 npm install --legacy-peer-deps
 npm run build
